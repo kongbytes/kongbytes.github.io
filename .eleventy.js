4859248963d2ebd6e3f14811b04e09ade0f8a83e
Saluki <saluki@gmx.fr>
2022-06-15 03:28:19 +0200

    Changing to docs folder

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,7 @@ module.exports = function (eleventyConfig) {
     return {
         dir: {
             input: 'source',
-            output: 'build'
+            output: 'docs'
         }
     }
 };
